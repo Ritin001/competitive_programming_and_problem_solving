7c0106e13a5fce218a3c52582d5fd6a32a3c7141
Ritin <ritin15032006@gmail.com>
2026-01-12 09:58:17 +0530

    lab 2

---
+++ lab-2/modulo_division.py
@@ -0,0 +1,8 @@
+io,md=input("enter the no of numbers and modulo value").split()
+sum=0
+print("enter the numbers")
+num = list(map(int, input().split()))
+for i in num:
+    sum = sum + i
+modulo = int(sum) % int(md)
+print(modulo)
