65f55b657717f9509a7e1b44eeac4987c1ac297a
Ritin <ritin15032006@gmail.com>
2026-01-19 09:22:36 +0530

    lab 3 part 1

---
+++ lab-3/modullar_power_sum.py
@@ -0,0 +1,6 @@
+num,modulo=map(int,input("enter the number,power,modulo").split())
+sum=0
+for i in range(num):
+    a,b=map(int,input("enter the numbers and the power").split())
+    sum =sum + a**b
+print(sum%1000)
