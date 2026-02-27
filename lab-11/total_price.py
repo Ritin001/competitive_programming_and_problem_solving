8fa5116bd965120c4145adf6147dedf998207147
Ritin <ritin15032006@gmail.com>
2026-02-27 09:00:45 +0530

    lab 11

--- lab-11/total_price.py
+++ lab-11/total_price.py
@@ -1,6 +1,12 @@
-n=int(input("enter the input"))
-a= list(map(int,input("enter the numbers").split()))
-for i in a:
-    print(i,end=" ")
-print()
-print("total price : ",sum(a))
+n=int(input())
+arr=list(map(int,input().split()))
+left=0
+right=n-1
+while(left<=right):
+    if (left==right):
+        print(arr[left])
+    else:
+        print(arr[left],arr[right])
+    left+=1
+    right-=1
+    
